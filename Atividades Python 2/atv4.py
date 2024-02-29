fad0525b57c42e0f96dcd94f82fac69160533687
Izah Lunardi <124892902+azinardi@users.noreply.github.com>
2024-02-29 11:55:40 -0300

    Add files via upload

---
+++ Atividades Python 2/atv4.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+num3 = int(input("Digite o terceiro número:"))
+
+maior = max(num1, num2, num3)
+menor = min(num1, num2, num3)
+meio = (num1 + num2 + num3) - maior - menor
+
+print("Números em ordem decrescente:")
+print(maior)
+print(meio)
+print(menor)  
